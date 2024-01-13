d266e75fca4542ebcbb334e0d6e657a768b74220
Minyoung Chung <minyoung.cng@gmail.com>
2024-01-12 19:32:34 -0500

    second

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const api_key = "912f02487b0a4670a7e91e403fc4f072";
 
@@ -9,27 +9,27 @@ const api_key = "912f02487b0a4670a7e91e403fc4f072";
  */
 export const fetchData = function (URL, callback) {
   fetch(`${URL}&appid=${api_key}`)
-    .then(res => res.json())
-    .then(data => callback(data));
-}
-
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
+//hello
 export const url = {
   currentWeather(lat, lon) {
-    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
   },
   forecast(lat, lon) {
-    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
   },
   airPollution(lat, lon) {
-    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
   },
   reverseGeo(lat, lon) {
-    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
   },
   /**
    * @param {string} query Search query e.g.: "London", "New York"
    */
   geo(query) {
-    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
-  }
-}
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
